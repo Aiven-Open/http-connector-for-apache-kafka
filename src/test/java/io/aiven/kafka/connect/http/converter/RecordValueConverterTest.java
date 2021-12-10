63e62b89659a3cf285a5a509f34e377d1323645f
Josep Prat <josep.prat@aiven.io>
2021-12-10 14:31:12 +0100

    Change header to Aiven and contributors, also allow copyright year ranges

--- src/test/java/io/aiven/kafka/connect/http/converter/RecordValueConverterTest.java
+++ src/test/java/io/aiven/kafka/connect/http/converter/RecordValueConverterTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Aiven Oy
+ * Copyright 2021 Aiven Oy and http-connector-for-apache-kafka project contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
