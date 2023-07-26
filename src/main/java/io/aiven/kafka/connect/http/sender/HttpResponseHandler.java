3ccdf0c7ebcb280fed2df78343ddf2a12e9f9083
Arnaud Prod'homme <arnaud.prodhomme@decathlon.com>
2023-07-26 11:51:34 +0200

    nit

--- src/main/java/io/aiven/kafka/connect/http/sender/HttpResponseHandler.java
+++ src/main/java/io/aiven/kafka/connect/http/sender/HttpResponseHandler.java
@@ -26,7 +26,7 @@ interface HttpResponseHandler {
 
     Logger LOGGER = LoggerFactory.getLogger(HttpResponseHandler.class);
 
-    void onResponse(final HttpResponse<String> response, int retriesNumber) throws IOException;
+    void onResponse(final HttpResponse<String> response, int remainingRetries) throws IOException;
 
     HttpResponseHandler ON_HTTP_ERROR_RESPONSE_HANDLER = (response, remainingRetries) -> {
         if (response.statusCode() >= 400) {
